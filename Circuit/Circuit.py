7a31b32cfc20770f36b5dece7d73d827bee6ce5f
Chimdi-O <chimdioguejiofor58@example.com>
2026-06-13 20:30:38 +0100

    removed node map from OperatingPoint added it to Circuit

--- Circuit/Circuit.py
+++ Circuit/Circuit.py
@@ -2,9 +2,17 @@
 class Circuit(): 
     def __init__(self):
         self.components = [] 
+        self.node_map = {} 
 
     def addComponent(self,comp): 
         self.components.append(comp)
+
+        for i in comp.nodes: 
+            if i != "0" and i not in self.node_map: 
+                if len(self.node_map) == 0: 
+                    self.node_map[i] = 1
+                else: 
+                    self.node_map[i] = len(self.node_map) 
 
     def __repr__(self): 
         netlist = []
